3ce4ff9f9a93770634912bc00b074476a07e45a3
ArielJX <asblackcat@hotmail.com>
2022-09-15 12:31:09 +1200

    updated backend

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 
-const dbURI = 'mongodb+srv://admin:yoobee123456@nodetuts.elfpghz.mongodb.net/CellSell?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://admin:yoobee123456@cluster0.emeo5tx.mongodb.net/CellSell?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }, function () { console.log("connection attempt") })
 mongoose.connection.on("error", function (e) { console.log(e) })
 mongoose.connection.on("connected", function (e) { console.log("successfully connected to database") })
