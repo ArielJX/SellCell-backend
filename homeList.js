3d3a750105b0c22f8a245cd8d039ae82d83df5cc
Darlene-uxui <darlene.uxui@gmail.com>
2022-09-16 10:22:06 +0900

    homeList backend js and server

--- homeList.js
+++ homeList.js
@@ -13,5 +13,3 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model('Product', productSchema);
 module.exports = Product;
-
-
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const UserMessage = require("./userMessage");
 const UserProfile = require('./userProfile')
 const Product = require("./product");
+const homeList = require("./homeList");
 const mongoose = require('mongoose');
 const app = express();
 const cors = require("cors");
@@ -14,11 +15,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 
-const dbURI = 'mongodb+srv://graceyoobee:crystal123@nodetuts.elfpghz.mongodb.net/node-tuts?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://dkoo9707:darlene@cluster0.6wm3prd.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }, function (){console.log("connection attempt")})
 mongoose.connection.on("error", function (e) {console.log(e)})
 mongoose.connection.on("connected", function (e) {console.log("successfully connected to database")})
-
 
 app.get('/userProfile', (req, res) => {
     UserProfile.find({}, function (error, result) {
@@ -29,6 +29,17 @@ app.get('/userProfile', (req, res) => {
         }
     })
 });
+
+app.get('/homeList', (req, res) => {
+    Product.find({}, function (error, result) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.json(result);
+        }
+    })
+});
+
 
 app.get('/products', (req, res) => {
     Product.find({}, function (error, result) {
@@ -104,17 +115,6 @@ app.delete('/products/:id', (req, res) => {
 });
 
 
-app.get('/products', (req, res) => {
-    Product.find({}, function (error, result) {
-        if (error) {
-            console.log(error);
-        } else {
-            res.json(result);
-        }
-    }) 
-    });
-
-
 app.get('/register', (req, res) => {
         UserProfile.find({}, function (error, result) {
             if (error) {
@@ -130,17 +130,18 @@ app.post('/register', (req, res) => {
     let { 
         username, 
         email,
-         password,
-          confirm_password
-         } = req.body
-         if(password !== confirm_password) {
+            password,
+        confirm_password
+        } = req.body
+        if(password !== confirm_password) {
             return res.status(400).json({
                 msg: "Password do not match."
             });
-         }
-          
+        }
+        
+
          //Check for the unique Username
-         UserProfile.findOne({
+        UserProfile.findOne({
             username: username
         }).then(user => {
             if (user){
@@ -148,9 +149,9 @@ app.post('/register', (req, res) => {
                     msg: "Username is already taken."
                 });
             }
-         })
+        })
          //check for the Unique Email
-         UserProfile.findOne({
+        UserProfile.findOne({
             email: email
         }).then(user => {
             if (user) {
@@ -158,12 +159,12 @@ app.post('/register', (req, res) => {
                     msg: "Email is already registered. Did you forget your password?"
                 });
             }
-         });
+        });
          // The data is valid and now we can register the user
-         let newUser = new UserProfile(req.body);
+        let newUser = new UserProfile(req.body);
 
-         newUser.save()
-         .then((result) => {
+        newUser.save()
+        .then((result) => {
             res.redirect('/login'); 
         })
         .catch((err) => {
@@ -174,7 +175,7 @@ app.post('/register', (req, res) => {
 
 
          //Hash the password
-         bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(newUser.password, salt, (err, hash) => {
                 if(err) throw err;
                 newUser.password = hash;
@@ -185,7 +186,7 @@ app.post('/register', (req, res) => {
                     });
                 });
             });
-         });
+        });
 });
 
 
